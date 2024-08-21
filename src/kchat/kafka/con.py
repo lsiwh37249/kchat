c82c89396af6f2b097bd38193f3b019276c1a263
lsiwh37249 <lsiwh37249@gmail.com>
2024-08-21 16:04:00 +0900

    enable_auto_commit by GROUP

--- src/kchat/kafka/con.py
+++ src/kchat/kafka/con.py
@@ -5,11 +5,16 @@ consumer =  KafkaConsumer(
         "kchat",
         bootstrap_servers=['localhost:9092'],
         value_deserializer = lambda x: loads(x.decode('utf-8')),
-        #consumer_timeout_ms=5000
+        consumer_timeout_ms=15000,
+        #auto_offset_reset='earliest',
+        auto_offset_reset='latest', # 'earliest','latest'
+        group_id="fbi",
+        enable_auto_commit=True,
+
 )
 
 print('[Start] get consumer')
-for msg in consumer:
-    print(msg)
+for m in consumer:
+    print(f"topic={m.topic}, offset={m.offset} ")
 print('[End] get consumer')
 
