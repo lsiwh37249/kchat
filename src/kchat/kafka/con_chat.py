41eb7bad4cc179ca4a4f6c24ccfdfd3c640be7d1
lsiwh37249 <lsiwh37249@gmail.com>
2024-08-21 18:24:15 +0900

    con_chat 변경 완료

--- src/kchat/kafka/con_chat.py
+++ src/kchat/kafka/con_chat.py
@@ -7,7 +7,7 @@ consumer = KafkaConsumer(
         auto_offset_reset='earliest',
         enable_auto_commit=True,
         group_id='chat-group',
-        value_deserializer=lambda x: loads(x.decode('utf-8'))
+        value_deserializer=lambda x: loads(x.decode('utf8'))
         
 )
 
